52933cc8dfe65d091b3a18abb160be343ecb6424
gladiussdv <gladiussdv@gmail.com>
2020-11-10 10:39:58 +0200

    landing page fix

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,7 +1,8 @@
 import { PortableText } from '../utils/sanity'
 import RenderSections from './RenderSections'
+import { Section } from "../models/Section";
 
-function LandingPage ({ page = {} }) {
+function LandingPage ({ page = {} }: {page: {title?: string, content?: Section[]}}) {
   const { title, content = [] } = page
   return <RenderSections sections={content} />
 }
